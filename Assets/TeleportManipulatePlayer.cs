b96c04cd2ff87619e79e30de51ddd85c8eb31db1
unknown <Daniel@F073PC.hagenberg.fhooe.at>
2016-05-03 17:45:19 +0200

    squeezer cone, light count to 64, stuff

---
+++ Assets/TeleportManipulatePlayer.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TeleportManipulatePlayer : MonoBehaviour {
+
+    void TeleporterEntered(GameObject player)
+    {
+        Rigidbody body = player.GetComponent<Rigidbody>();
+
+        if (body != null)
+        {
+            body.velocity = Vector3.zero;
+        }
+
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
