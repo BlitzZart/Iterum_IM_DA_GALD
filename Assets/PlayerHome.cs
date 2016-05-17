04ec15eb590f58923e2a3c3a60919fe429c8e9f8
unknown <dnl.rammer@gmail.com>
2016-05-17 12:26:20 +0200

    added new tower

--- Assets/PlayerHome.cs
+++ Assets/PlayerHome.cs
@@ -1,6 +1,9 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+/// <summary>
+/// used as parent for the player transform - when not assigned to other rooms
+/// </summary>
 public class PlayerHome : MonoBehaviour {
     private static PlayerHome instance;
     public static PlayerHome Instance { get { return instance; } }
