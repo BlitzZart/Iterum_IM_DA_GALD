fdd04155fe806112887003fa3d0d2d6cd445e39b
unknown <dnl.rammer@gmail.com>
2016-05-30 13:22:40 +0200

    applied squeezer models + theDoor models

--- Assets/Scripts/WallRotator.cs
+++ Assets/Scripts/WallRotator.cs
@@ -3,8 +3,8 @@ using Triggers;
 
 public class WallRotator : MonoBehaviour {
 
-    private int runingCW = -90;
-    private int runingCCW = -90;
+    private int runingCW = 90;
+    private int runingCCW = 90;
 
     public GameObject wall;
 
@@ -14,11 +14,11 @@ public class WallRotator : MonoBehaviour {
 
     void TriggerChanged(TriggerType changedTo) {
         if (changedTo == TriggerType.A) { // CCW
-            wall.transform.Rotate(0, runingCW, 0, Space.Self);
+            wall.transform.Rotate(0, 0, runingCW, Space.Self);
             //wall.transform.RotateAround(transform.parent.localPosition, new Vector3(0, 1, 0), runingCW);
         }
         else if (changedTo == TriggerType.B) { // CW
-            wall.transform.Rotate(0, runingCCW, 0, Space.Self);
+            wall.transform.Rotate(0, 0, runingCCW, Space.Self);
             //wall.transform.RotateAround(transform.parent.localPosition, new Vector3(0, 1, 0), runingCW);
         }
     }
