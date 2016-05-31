f056391c253b12c51b8d67c6225d5668bab8110f
unknown <dnl.rammer@gmail.com>
2016-05-31 11:01:24 +0200

    fixed squeezer bug, walls an wrong position at end od puzzle

--- Assets/Scripts/Squeezer/SqueezerEndState.cs
+++ Assets/Scripts/Squeezer/SqueezerEndState.cs
@@ -3,18 +3,28 @@ using System.Collections;
 
 public class SqueezerEndState : MonoBehaviour {
 
+    public SqueezeWall wallStart, wallAhead;
+
+    private bool doneOnce = false;
+
     // Use this for initialization
     void Start() {
 
     }
     void MsgPlayerEntered(GameObject player) {
-        SqueezeWall[] walls = FindObjectsOfType<SqueezeWall>();
-
-        foreach(SqueezeWall wall in walls) {
-            wall.DeActivate();
-        }
+        //SqueezeWall[] walls = FindObjectsOfType<SqueezeWall>();
+        //foreach(SqueezeWall wall in walls) {
+        //    wall.DeActivate();
+        //}
 
         // push back wall
-        walls[1].transform.Translate(0, -2, 0);
+        if (doneOnce)
+            return;
+
+        doneOnce = true;
+        wallStart.DeActivate();
+        wallAhead.DeActivate();
+
+        wallAhead.transform.Translate(0, -2, 0);
     }
 }
