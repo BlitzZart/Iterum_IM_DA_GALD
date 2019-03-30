13f53b2c336ede645d709698f48137a7fe1fab3a
redovic <dnl.rammer@gmail.com>
2019-03-30 23:33:15 +0100

    optimized vr version (game and menu)

---
+++ Assets/FadeMaterials.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+public class FadeMaterials : MonoBehaviour
+{
+    private Renderer m_renderer;
+
+    private void Start()
+    {
+        m_renderer = GetComponent<Renderer>();
+    }
+
+    public void ShortBlack()
+    {
+        iTween.ValueTo(gameObject, iTween.Hash(
+            "from", 0.0f, "to", 1.0f,
+            "time", 0.1f, "easetype", "linear",
+            "onupdate", "setAlpha"));
+
+        iTween.ValueTo(gameObject, iTween.Hash(
+            "from", 1.0f, "to", 0.0f,
+            "time", 0.2f, "easetype", "linear",
+            "onupdate", "setAlpha"));
+    }
+    public void FadeOut(float duration = 3.0f)
+    {
+        iTween.ValueTo(gameObject, iTween.Hash(
+            "from", 0.0f, "to", 1.0f,
+            "time", duration, "easetype", "linear",
+            "onupdate", "setAlpha"));
+    }
+    public void FadeIn(float duration = 3.0f)
+    {
+        iTween.ValueTo(gameObject, iTween.Hash(
+            "from", 1.0f, "to", 0.0f,
+            "time", duration, "easetype", "linear",
+            "onupdate", "setAlpha"));
+    }
+
+    public void setAlpha(float newAlpha)
+    {
+        foreach (Material mObj in m_renderer.materials)
+        {
+            mObj.color = new Color(
+                mObj.color.r, mObj.color.g,
+                mObj.color.b, newAlpha);
+        }
+    }
+
+}
---
+++ Assets/Scripts/VrFpsControl.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class VrFpsControl : MonoBehaviour
+{
+
+    public FadeMaterials toBlack;
+
+    private bool m_readyToRotate = true;
+    private float m_deadZone = 0.1f;
+
+    private float m_turnStep = 45.0f;
+
+    private FirstPersonController m_fpsCtrl;
+
+    // Use this for initialization
+    void Start()
+    {
+        m_fpsCtrl = GetComponent<FirstPersonController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // win
+        float turn = Input.GetAxis("RightStickX_Win");
+        // mac
+        //float turn = Input.GetAxis("RightStickX_Mac");
+
+        if (Mathf.Abs(turn) < m_deadZone)
+        {
+            m_readyToRotate = true;
+        }
+
+        if (m_readyToRotate && Mathf.Abs(turn) > m_deadZone)
+        {
+            m_readyToRotate = false;
+
+            if (turn > 0)
+            {
+                toBlack.ShortBlack();
+                StopAllCoroutines();
+                StartCoroutine(Turn(m_turnStep));
+            }
+            else
+            {
+                toBlack.ShortBlack();
+                StopAllCoroutines();
+                StartCoroutine(Turn(-m_turnStep));
+            }
+        }
+
+
+    }
+    private IEnumerator Turn(float dir)
+    {
+        toBlack.FadeOut(0.05f);
+        yield return new WaitForSeconds(0.05f);
+        m_fpsCtrl.RotateBy(dir);
+        toBlack.FadeIn(0.15f);
+    }
+
+}
